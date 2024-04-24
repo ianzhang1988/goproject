2fcb21fae948eed6e021541ed43398fa3413a15f
zhangyang08 <zhangyang08@qiyi.com>
2024-04-24 20:45:12 +0800

    add kcp test

---
+++ atwork/udp_sender/main.go
@@ -0,0 +1,244 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/xtaci/kcp-go"
+	"golang.org/x/crypto/pbkdf2"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func StringWithCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+var (
+	server     string
+	messageNum = 7000
+	// messageNum = 7
+	id         string
+	httpServer string
+	interval   int
+	kcpTimeout int
+)
+
+func httpSend() {
+	dummyData := StringWithCharset(900, charset)
+	format := `{"counter":%d, "id":"%s", "data":"%s"}`
+
+	counter := 0
+
+	client := &http.Client{}
+
+	for {
+
+		batchCounter := 0
+		PBatchCounter := &batchCounter
+
+		start := time.Now()
+
+		for i := 0; i < messageNum; i++ {
+			pI := &i
+
+			func() {
+				data := fmt.Sprintf(format, counter, id, dummyData)
+				req, err := http.NewRequest("POST", httpServer, bytes.NewBuffer([]byte(data)))
+				if err != nil {
+					// fmt.Println("Error creating request: ", err)
+					logrus.WithFields(logrus.Fields{
+						"err": err,
+					}).Info("http creating request")
+					return
+				}
+
+				req.Header.Set("Content-Type", "application/json")
+
+				resp, err := client.Do(req)
+				if err != nil {
+					// fmt.Println("Error making request: ", err)
+					(*pI)--
+					logrus.WithFields(logrus.Fields{
+						"err": err,
+					}).Info("http request")
+					return
+				}
+				defer resp.Body.Close()
+
+				if resp.StatusCode != 200 {
+					(*pI)--
+					logrus.WithFields(logrus.Fields{
+						"err": err,
+					}).Info("http request")
+				}
+
+				_, _ = ioutil.ReadAll(resp.Body)
+
+				(*PBatchCounter)++
+				counter++
+
+			}()
+		}
+
+		logrus.WithFields(logrus.Fields{
+			"num": batchCounter,
+		}).Info("http write")
+
+		timeuse := time.Since(start)
+		wait := time.Duration(interval) * time.Second
+		if timeuse < wait {
+			sleep := wait - timeuse
+			time.Sleep(sleep)
+		}
+	}
+}
+
+func initLog() {
+
+	// log := logrus.New()
+
+	// Log as JSON instead of the default ASCII formatter.
+	// logrus.SetFormatter(&logrus.JSONFormatter{})
+
+	// Keep log files to maximum size 5 MB
+	logrus.SetOutput(io.MultiWriter(
+		&lumberjack.Logger{
+			Filename:   "my.log",
+			MaxSize:    5, // megabytes
+			MaxBackups: 5,
+			MaxAge:     28, //days
+		},
+		os.Stdout))
+}
+
+func main() {
+	flag.StringVar(&server, "s", "", "server")
+	flag.StringVar(&id, "id", "test", "")
+	flag.StringVar(&httpServer, "http", "", "http server")
+	flag.IntVar(&interval, "n", 300, "interval")
+	flag.IntVar(&kcpTimeout, "kt", 3, "kcp timeout")
+	flag.IntVar(&messageNum, "msg", 7000, "msg num")
+
+	flag.Parse()
+
+	initLog()
+
+	if httpServer != "" {
+		go httpSend()
+	}
+
+	dummyData := StringWithCharset(900, charset)
+	format := `{"counter":%d, "id":"%s", "data":"%s"}`
+
+	counter := 0
+
+	key := pbkdf2.Key([]byte("zhangyang"), []byte("zhangyang salt"), 1024, 16, sha1.New)
+	block, _ := kcp.NewAESBlockCrypt(key)
+
+	if sess, err := kcp.DialWithOptions(server, block, 10, 3); err == nil {
+		// fmt.Println("local: ", sess.LocalAddr())
+		logrus.WithFields(logrus.Fields{
+			"addr": sess.LocalAddr(),
+		}).Info("local")
+		defer sess.Close()
+		for {
+
+			var err error
+			failed := false
+
+			start := time.Now()
+
+			batchCounter := 0
+
+			for i := 0; i < messageNum; i++ {
+
+				data := fmt.Sprintf(format, counter, id, dummyData)
+
+				buf := make([]byte, 10)
+				//fmt.Println("sent:", data)
+				sess.SetDeadline(time.Now().Add(time.Duration(kcpTimeout) * time.Second))
+				if _, err = sess.Write([]byte(data)); err == nil {
+					if n, err := sess.Read(buf); err == nil {
+						// fmt.Println("recv:", string(buf))
+						if string(buf[:n]) != "OK" {
+							logrus.WithFields(logrus.Fields{
+								"buf": string(buf[:n]),
+							}).Info("read not OK")
+						}
+					} else {
+						// fmt.Println("read err: ", err)
+						logrus.WithFields(logrus.Fields{
+							"err": err,
+						}).Info("read")
+						failed = true
+
+					}
+				} else {
+					// fmt.Println("write err:", err)
+					logrus.WithFields(logrus.Fields{
+						"err": err,
+					}).Info("write")
+					failed = true
+				}
+
+				if failed {
+					sess.Close()
+
+					for {
+						sess, err = kcp.DialWithOptions(server, block, 10, 3)
+						if err != nil {
+							// fmt.Println("redial err: ", err)
+							logrus.WithFields(logrus.Fields{
+								"err": err,
+							}).Info("redial")
+							time.Sleep(5 * time.Second)
+						} else {
+							// fmt.Println("redial success local: ", sess.LocalAddr())
+							logrus.WithFields(logrus.Fields{
+								"addr": sess.LocalAddr(),
+							}).Info("redial success")
+							failed = false
+
+							break
+						}
+					}
+
+					i--
+					counter--
+				}
+
+				counter += 1
+				batchCounter += 1
+			}
+
+			logrus.WithFields(logrus.Fields{
+				"num": batchCounter,
+			}).Info("write")
+
+			timeuse := time.Since(start)
+			wait := time.Duration(interval) * time.Second
+			if timeuse < wait {
+				sleep := wait - timeuse
+				time.Sleep(sleep)
+			}
+		}
+	} else {
+		panic(err)
+	}
+}
---
+++ atwork/udp_server/main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"crypto/sha1"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/xtaci/kcp-go"
+	"golang.org/x/crypto/pbkdf2"
+)
+
+type MyMsg struct {
+	Counter int    `json:"counter"`
+	Id      string `json:"id"`
+}
+
+type LogstashMsg struct {
+	Time time.Time `json:"@timestamp"`
+	Msg  string    `json:"message"`
+}
+
+var (
+	id       string
+	interval int
+)
+
+func httpServer() {
+
+	counterChan := make(chan MyMsg, 10000)
+
+	go countMsg(counterChan, "http")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data, err := io.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println("http read err: ", err)
+		}
+		defer r.Body.Close()
+
+		msg := MyMsg{}
+		err = json.Unmarshal(data, &msg)
+		if err != nil {
+			// fmt.Printf("json err: %s\n", err)
+			w.WriteHeader(http.StatusTeapot)
+			return
+		}
+
+		counterChan <- msg
+	})
+
+	http.ListenAndServe(":9204", nil)
+}
+
+func handle(conn *kcp.UDPSession, out chan MyMsg) {
+	defer conn.Close()
+	buf := make([]byte, 4096)
+
+	// counter := 0
+
+	for {
+		conn.SetDeadline(time.Now().Add(11 * time.Minute))
+
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		msg := MyMsg{}
+		err = json.Unmarshal(buf[:n], &msg)
+		if err != nil {
+			// fmt.Printf("json err: %s\n", err)
+			continue
+		}
+
+		// fmt.Println("client counter: ", msg.Counter)
+
+		_, err = conn.Write([]byte("OK"))
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		out <- msg
+	}
+
+	fmt.Println("handle close: ", conn.RemoteAddr().String())
+}
+
+func countMsg(input chan MyMsg, proto string) {
+	start := time.Now()
+	counter := 0
+
+	lock := sync.Mutex{}
+
+	t := time.NewTicker(time.Duration(interval) * time.Second)
+	go func() {
+		for _ = range t.C {
+			start = time.Now()
+			lock.Lock()
+			fmt.Printf("%s time: %v, counter: %d\n", proto, start, counter)
+			counter = 0
+			lock.Unlock()
+		}
+	}()
+
+	msgCounterExpect := 0
+
+	for msg := range input {
+
+		if msg.Counter != msgCounterExpect {
+			fmt.Printf("%s counter miss, expect %d, actual: %d\n", proto, msgCounterExpect, msg.Counter)
+			msgCounterExpect = msg.Counter + 1
+		} else {
+			msgCounterExpect += 1
+		}
+
+		lock.Lock()
+		counter += 1
+		lock.Unlock()
+	}
+}
+
+func main() {
+
+	flag.IntVar(&interval, "n", 300, "interval")
+	flag.StringVar(&id, "id", "test", "")
+	flag.Parse()
+
+	go httpServer()
+
+	key := pbkdf2.Key([]byte("zhangyang"), []byte("zhangyang salt"), 1024, 16, sha1.New)
+	block, _ := kcp.NewAESBlockCrypt(key)
+
+	counterChan := make(chan MyMsg, 10000)
+	defer func() {
+		close(counterChan)
+	}()
+
+	go countMsg(counterChan, "udp")
+
+	if listener, err := kcp.ListenWithOptions("0.0.0.0:9203", block, 10, 3); err == nil {
+		for {
+			s, err := listener.AcceptKCP()
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("start conn: ", s.RemoteAddr().String())
+			go handle(s, counterChan)
+		}
+	} else {
+		panic(err)
+	}
+
+}
